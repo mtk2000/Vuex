5c3ae13253ec29dc828e65ea75c6c84b742dad3e
pengjinghuan <pengjinghuan@sinohealth.cn>
2021-07-21 20:33:28 +0800

    mm

--- src/main.js
+++ src/main.js
@@ -1,13 +1,17 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
+import Vue from 'vue';
+import App from './App';
 
-Vue.config.productionTip = false
+// 引入store
+import store from './store.js';
+
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
+  store, // store 挂载到根组件下
   components: { App },
   template: '<App/>'
-})
+});
