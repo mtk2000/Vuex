c92789812551149be82e5e35f7f2f851ffe51808
pengjinghuan <pengjinghuan@sinohealth.cn>
2021-07-21 20:05:26 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <my-addition></my-addition>
+    <p>----------------</p>
+    <my-subtraction></my-subtraction>
+  </div>
+</template>
+
+<script>
+import Addition from './components/Addition.vue';
+import Subtraction from './components/Subtraction.vue';
+export default {
+  name: 'App',
+  components: {
+    'my-addition': Addition,
+    'my-subtraction': Subtraction
+  },
+  data() {
+    return {};
+  }
+};
+</script>
