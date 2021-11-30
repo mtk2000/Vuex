45278646725228a6bb6491975fc7143a31ddf6d9
pengjinghuan <pengjinghuan@sinohealth.cn>
2021-11-30 16:53:31 +0800

    mm

--- src/main.js
+++ src/main.js
@@ -1,5 +1,3 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue';
 import App from './App';
 
@@ -8,14 +6,13 @@ import store from './store.js';
 
 Vue.config.productionTip = false;
 
-// 创建 Vue 实例之前定义全局过滤器
+// 创建 Vue 实例之前自定义全局过滤器
 Vue.filter('globalCapitalize', function(value) {
   if (!value) return '';
   value = value.toString();
   return value.charAt(0).toUpperCase() + value.slice(1) + '-haha';
 });
 
-/* eslint-disable no-new */
 new Vue({
   el: '#app',
   store, // store 挂载到根组件下
